57053dba5e886e6d3b0cf24c9afa7179d28af660
Qwaz <qwazpia@gmail.com>
2024-08-09 05:51:52 +0900

    Update Rust aliases

--- optional/rust.sh
+++ optional/rust.sh
@@ -2,18 +2,18 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source $HOME/.cargo/env
 cargo install --locked bat
-cargo install --locked exa
+cargo install --locked eza
 
 RC_PATH="${RC_PATH}:\$HOME/.cargo/bin"
 
-str='alias ls=exa
-alias la="exa -a"
-alias ll="exa -l"
-alias lal="exa -al"
-alias lla="exa -la"
-alias lr="exa -lT"
+str='alias ls=eza
+alias la="eza -a"
+alias ll="eza -l"
+alias lal="eza -al"
+alias lla="eza -la"
+alias lr="eza -lT"
 
-alias cat="bat -p --paging never"
+alias cat="bat -p --paging=never"
 '
 RC="$RC
 $str"
